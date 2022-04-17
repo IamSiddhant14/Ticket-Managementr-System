3b8c7211f598c3488ea58da2befe7e6a36167ae7
Siddhant <siddhantsharma669@gmail.com>
2022-04-17 07:39:51 +0530

    Created ticket

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let addBtn = document.querySelector('.add-btn')
+let flag = true;
 
 addBtn.addEventListener('click' , function(e){
        // Display a Modal
-
-
-
+       let addcard = document.querySelector('.modal-cont')
+       if( flag ){
+              addcard.style.display = "flex"
+              flag = false;
+       }else{
+              addcard.style.display = "none"
+              flag = true;
+       }
 
 
        // Add a Card
