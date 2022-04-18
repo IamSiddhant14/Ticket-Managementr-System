001cc7a9e45152168da65220a3d4b95d35dc1193
Siddhant <siddhantsharma669@gmail.com>
2022-04-19 01:13:29 +0530

    Added dyamic apperance of ticket

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-let addBtn = document.querySelector('.add-btn')
+let addBtn = document.querySelector(".add-btn");
+
+let modalCont = document.querySelector(".modal-cont");
+
+let mainCont = document.querySelector('.main-cont')
+
 let flag = true;
 
-addBtn.addEventListener('click' , function(e){
-       // Display a Modal
-       let addcard = document.querySelector('.modal-cont')
+addBtn.addEventListener("click", function (e) {
+       //Display the Modal
+
        if( flag ){
-              addcard.style.display = "flex"
+              modalCont.style.display = "flex"
               flag = false;
        }else{
-              addcard.style.display = "none"
+              modalCont.style.display = "none"
               flag = true;
        }
 
-
-       // Add a Card
-
+});
 
 
+
+// Generating a Ticket
+
+modalCont.addEventListener('keydown', function (e) {
+       let key = e.key
+
+       if (key == 'Shift') {
+              createTicket() // this function will generate the ticket
+              modalCont.style.display = 'none'
+       }
 })
+
+
+
+function createTicket() {
+       let ticketCont = document.createElement('div')
+       ticketCont.setAttribute('class', 'ticket-cont')
+
+       ticketCont.innerHTML = `   
+       <div class="ticketcolor"></div>
+       <div class="ticket-id"></div>
+       <div class="task-area"></div>
+  `
+
+       mainCont.appendChild(ticketCont)
+
+
+}
