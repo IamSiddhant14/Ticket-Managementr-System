0dfa476888b4929b14ce2ceac79e02719f0989a6
Siddhant <siddhantsharma669@gmail.com>
2022-04-19 02:13:47 +0530

    Added color property to the ticket

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let addBtn = document.querySelector(".add-btn");
 
 let modalCont = document.querySelector(".modal-cont");
 
-let mainCont = document.querySelector('.main-cont')
+let mainCont = document.querySelector('.main-cont');
+
+let colors = ['lightpink' , 'lightblue' , 'lightgreen' , 'black']
+
+let modalPriorityColor = colors[colors.length-1] // black
+
+let allPriorityColors = document.querySelectorAll('.priority-color')
 
 let flag = true;
 
@@ -27,19 +33,19 @@ modalCont.addEventListener('keydown', function (e) {
        let key = e.key
 
        if (key == 'Shift') {
-              createTicket() // this function will generate the ticket
+              createTicket(modalPriorityColor) // this function will generate the ticket
               modalCont.style.display = 'none'
        }
 })
 
 
 
-function createTicket() {
+function createTicket(ticketKaColorClass) {
        let ticketCont = document.createElement('div')
        ticketCont.setAttribute('class', 'ticket-cont')
 
        ticketCont.innerHTML = `   
-       <div class="ticketcolor"></div>
+       <div class="ticketcolor ${ticketKaColorClass}"></div>
        <div class="ticket-id"></div>
        <div class="task-area"></div>
   `
@@ -48,3 +54,18 @@ function createTicket() {
 
 
 }
+
+
+//Changing Priority Colors
+
+allPriorityColors.forEach(function(colorElem){
+       colorElem.addEventListener('click' , function(e){
+            allPriorityColors.forEach(function(priorityColorElem){
+               priorityColorElem.classList.remove('active')
+            })
+            colorElem.classList.add('active')
+
+            modalPriorityColor = colorElem.classList[0];
+       })
+     })
+     
